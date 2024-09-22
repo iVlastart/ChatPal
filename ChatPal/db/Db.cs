12fbb0b041903487af07a6e9e95b0a57db14d1c5
Nikča <136233480+NickieUwU@users.noreply.github.com>
2024-09-22 19:40:26 +0200

    Update Db.cs

--- ChatPal/db/Db.cs
+++ ChatPal/db/Db.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,29 +10,29 @@ namespace ChatPal.db
 {
     internal static class Db
     {
-        internal static void addUser(string ID, string username, string password)
-        {
-            SqlConnection con = new(Enviro.CONNECT());
-            try
-            {
-                openCon(con);
-                string query = "INSERT INTO Users (ID, Username, Password) VALUES (@ID, @username, @password)";
-                using(SqlCommand cmd = new(query, con))
-                {
-                    cmd.Parameters.AddWithValue("@ID", makeID(username));
+        internal static void addUser(string username, string password) 
+        { 
+            SqlConnection con = new(Enviro.CONNECT()); 
+            try 
+            { 
+                openCon(con); 
+                string query = "INSERT INTO Users (ID, Username, Password) VALUES (@ID, @username, @password)"; 
+                using (SqlCommand cmd = new(query, con)) 
+                { 
+                    cmd.Parameters.AddWithValue("@ID", makeID(username)); 
                     cmd.Parameters.AddWithValue("@username", username);
-                    cmd.Parameters.AddWithValue("@password", password);
-                    cmd.ExecuteNonQuery();
-                }
-                con.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "addUser ERROR", MessageBoxButton.OK);
-            }
-            finally
-            {
-                con.Close();
+                    cmd.Parameters.AddWithValue("@password", password); 
+                    cmd.ExecuteNonQuery(); 
+                } 
+                MessageBox.Show("Added"); 
+            } 
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message, "addUser ERROR", MessageBoxButton.OK); 
+            } 
+            finally 
+            { 
+                con.Close(); 
             } 
         }
 
@@ -45,21 +45,21 @@ namespace ChatPal.db
                 if (!checkIfUserExists(username))
                 {
                     string query = "SELECT * FROM Users WHERE Username=@username AND Password=@password";
-                    using(SqlCommand cmd = new(query, con))
+                    using (SqlCommand cmd = new(query, con))
                     {
                         cmd.Parameters.AddWithValue("@username", username);
                         cmd.Parameters.AddWithValue("@password", password);
-                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        using (SqlDataReader reader = cmd.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                
+
                             }
                         }
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "logUser ERROR", MessageBoxButton.OK);
             }
@@ -69,16 +69,15 @@ namespace ChatPal.db
             }
         }
 
-        //change this to private later after checking if it works
-        internal static string makeID(string username)
+        private static string makeID(string username)
         {
             Random random = new();
             var usernameBytes = System.Text.Encoding.UTF8.GetBytes(username);
-            var id1stPart =  System.Convert.ToBase64String(usernameBytes);
+            var id1stPart = System.Convert.ToBase64String(usernameBytes);
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             var id2ndPart = Enumerable.Repeat(chars, 10)
                 .Select(s => s[random.Next(s.Length)]).ToArray();
-            return $"{id1stPart}.{id2ndPart}";
+            return $"{id1stPart}.{new string(id2ndPart)}";
         }
 
         private static string checkErrors(string username, string password)
@@ -91,10 +90,10 @@ namespace ChatPal.db
             {
                 return "Please provide a password";
             }
-            /*else if()
+            else if(false)
             {
                 return $"{username} was not found";
-            }*/
+            }
             else
             {
                 return "";
@@ -108,28 +107,28 @@ namespace ChatPal.db
             {
                 openCon(con);
                 string query = "SELECT Username FROM Users WHERE Username=@username";
-                using(SqlCommand cmd = new(query, con))
+                using (SqlCommand cmd = new(query, con))
                 {
                     cmd.Parameters.AddWithValue("@username", username);
-                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         if (reader.HasRows) return true;
                         else return false;
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "findUserByUsername ERROR", MessageBoxButton.OK);
                 return false;
             }
             finally { con.Close(); }
-            
+
         }
 
         private static void openCon(SqlConnection con)
         {
-            if(con.State == System.Data.ConnectionState.Closed) con.Open();
+            if (con.State == System.Data.ConnectionState.Closed) con.Open();
         }
     }
 }
