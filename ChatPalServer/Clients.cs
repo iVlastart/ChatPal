24f9e2bba8d68b5de518fd599b93999424586bd9
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-03-29 15:18:13 +0100

    Working on server

---
+++ ChatPalServer/Clients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatPalServer
+{
+    public class Clients
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient Client { get; set; }
+        public Clients(TcpClient client)
+        {
+            this.Client = client;
+            this.UID = Guid.NewGuid();
+
+            Console.WriteLine($"{DateTime.Now}: {Client} has connected!");
+        }
+    }
+}
--- ChatPalServer/Program.cs
+++ ChatPalServer/Program.cs
@@ -5,15 +5,19 @@ namespace ChatPalServer
 {
     class Program
     {
+        static List<Clients> _users;
         static TcpListener _listener;
 
         static void Main(string[] args)
         {
+            _users = new List<Clients>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 6969);
             _listener.Start();
-
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected");
+            while(true)
+            {
+                var client = new Clients(_listener.AcceptTcpClient());
+                _users.Add(client);
+            }
         }
     }
 }
